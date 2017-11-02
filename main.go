72be203ebbe8134b515da20ce1083c04a8e375d0
Gildas Chabot <gildaschbt@gmail.com>
2017-11-02 20:48:52 +0000

    Time the execution the steps
    Results:
    
    Listening on 8080...
    url extract: 1.458µs
    client.Open: 1.960876299s
    io.Copy: 3.695846033s
    return: 3.695875491s
    url extract: 584ns
    client.Open: 167.741945ms
    io.Copy: 855.581858ms
    return: 855.614232ms
    url extract: 2.041µs
    client.Open: 191.172419ms
    io.Copy: 1.331846351s
    return: 1.331909642s
    url extract: 875ns
    client.Open: 193.953431ms
    io.Copy: 904.199799ms
    return: 924.904972ms

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 
 	"upspin.io/client"
 	"upspin.io/config"
@@ -21,19 +22,24 @@ func main() {
 	client := client.New(conf)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 		url := strings.TrimPrefix(r.URL.Path, "/")
+		fmt.Printf("url extract: %s\n", time.Since(start))
 		f, err := client.Open(upspin.PathName(url))
+		fmt.Printf("client.Open: %s\n", time.Since(start))
 		if err != nil {
 			fmt.Println(err)
 			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}
 		_, err = io.Copy(w, f)
+		fmt.Printf("io.Copy: %s\n", time.Since(start))
 		if err != nil {
 			fmt.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		fmt.Printf("return: %s\n", time.Since(start))
 	})
 
 	fmt.Println("Listening on 8080...")
