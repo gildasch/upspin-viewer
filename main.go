6bc326ac717187c22d91191a256f45ac7e65bce0
Gildas Chabot <gildaschbt@gmail.com>
2017-11-02 21:15:15 +0000

    If directory, list all files

--- main.go
+++ main.go
@@ -22,6 +22,27 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		url := strings.TrimPrefix(r.URL.Path, "/")
+
+		de, err := client.Lookup(upspin.PathName(url), true)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		if de.IsDir() {
+			des, err := client.Glob(upspin.AllFilesGlob(upspin.PathName(url)))
+			if err != nil {
+				fmt.Println(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			for _, dess := range des {
+				fmt.Fprintf(w, "<a href='/%s'>%s</a><br />", dess.SignedName, dess.SignedName)
+			}
+			return
+		}
+
 		f, err := client.Open(upspin.PathName(url))
 		if err != nil {
 			fmt.Println(err)
